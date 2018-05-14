2465b9822931a926d58c1e53c7aba24b2e98849c
Wellington Mateus <wellington.mateus@cmosdrake.com.br>
2018-05-14 14:54:02 -0300

    Versão Inicial

---
+++ Calculo/src/br/com/cmos/calculo/Calcula.java
@@ -0,0 +1,14 @@
+package br.com.cmos.calculo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Calcula {
+
+	
+	@Test
+	public void testeConfiguracao() {
+		Assert.assertEquals(2, (1+1), 0);
+	}
+
+}
